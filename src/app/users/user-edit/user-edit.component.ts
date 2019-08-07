f1591c6c17681bcc107cb9ec6e0b7548d6eb5053
Grégory Lecubin <gregory.lecubin@gmail.com>
2019-08-07 23:56:34 +0200

    User edit reactive form with Bootstrap validation

--- src/app/users/user-edit/user-edit.component.ts
+++ src/app/users/user-edit/user-edit.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { User } from '../user.model';
-import { FormGroup, FormControl, Validators } from '@angular/forms';
+import { FormGroup, Validators, FormBuilder } from '@angular/forms';
+import { MustMatch } from 'src/app/_helpers/must-match.validator';
 
 @Component({
     selector: 'app-user-edit',
@@ -11,8 +12,9 @@ export class UserEditComponent implements OnInit {
     public user: User;
     public titles = ['Madame', 'Mademoiselle', 'Monsieur'];
     userForm: FormGroup;
+    submitted = false;
 
-    constructor() {
+    constructor(private formBuilder: FormBuilder) {
         this.user = {
             id: null,
             username: null,
@@ -23,15 +25,40 @@ export class UserEditComponent implements OnInit {
     }
 
     ngOnInit() {
-        this.userForm = new FormGroup({
-            username: new FormControl(null, Validators.required), // on ne met pas required() car on veut juste une référence à la méthode
-            email: new FormControl(null, [Validators.required, Validators.email]),
-            title: new FormControl('Monsieur'),
-        });
+        // this.userForm = new FormGroup({
+        //     username: new FormControl(null, Validators.required), // on ne met pas required() car on veut juste une référence à la méthode
+        //     email: new FormControl(null, [Validators.required, Validators.email]),
+        //     title: new FormControl('Monsieur'),
+        // });
+        this.userForm = this.formBuilder.group(
+            {
+                title: ['', Validators.required],
+                firstname: ['', Validators.required],
+                lastname: ['', Validators.required],
+                username: ['', Validators.required],
+                email: ['', [Validators.required, Validators.email]],
+                password1: ['', [Validators.required, Validators.minLength(8)]],
+                password2: ['', Validators.required],
+            },
+            {
+                validator: MustMatch('password1', 'password2'),
+            },
+        );
         // console.log('test', this.user);
+    }
+
+    // convenience getter for easy access to form fields
+    get f() {
+        return this.userForm.controls;
     }
 
     onSubmit() {
         console.log(this.userForm);
+        this.submitted = true;
+
+        // stop here if form is invalid
+        if (this.userForm.invalid) {
+            return;
+        }
     }
 }
